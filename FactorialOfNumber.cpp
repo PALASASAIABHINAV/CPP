9f8922d53c7ffb3170548c55aaad6b12888711ad
Abhinav <psaiabhinav1724@gmail.com>
2025-06-11 23:56:12 +0530

    Factorial of n number

---
+++ FactorialOfNumber.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,fact=1;
+    cout<<"enter the n number:";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+      fact*=i;
+    }
+    cout<<fact;
+    return 0;
+}

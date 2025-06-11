c4787f3dead050ce1e7c47eecb4c63e6ace3832a
Abhinav <psaiabhinav1724@gmail.com>
2025-06-11 07:50:37 +0530

    Switch Case of Printing day by number

---
+++ SwitchCaseDay.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int day;
+    cout<<"Enter the day of today (1 to 7):";
+    cin>>day;
+    switch(day){
+        case 1:cout<<"Sunday";
+               break;
+        case 2:cout<<"Monday";
+               break;
+        case 3:cout<<"Thusday";
+               break;
+        case 4:cout<<"Wednesday";
+               break;
+        case 5:cout<<"Thrusday";
+               break;
+        case 6:cout<<"Friday";
+               break;
+        case 7:cout<<"Saturday";
+               break;
+        default:cout<<"The Input day is not correct";
+    }
+    return 0;
+}

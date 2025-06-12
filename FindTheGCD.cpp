dbaa9d70f443e7ba79102706c232129dea209b5a
Abhinav <psaiabhinav1724@gmail.com>
2025-06-12 19:31:48 +0530

    Find the GCD

---
+++ FindTheGCD.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cout<<"Enter the numbers";
+    cin>>n>>m;
+    while(m!=n){
+        if(m>n){
+            m-=n;
+        }
+        if(n>m){
+            n-=m;
+        }
+    }
+    
+    cout<<"The GCD(greatest common division (or) Factor):"<<m;
+
+    return 0;
+}

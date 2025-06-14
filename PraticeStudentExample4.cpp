9f426ca11b9f3dacd95d3831a9883f3c1570909f
Abhinav <psaiabhinav1724@gmail.com>
2025-06-14 23:50:22 +0530

    Pratice Student Example 4

--- PraticeStudentExample4.cpp
+++ PraticeStudentExample4.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int year;
-    cout<<"Enter the year:";
+    cout<<"Enter the year : ";
     cin>>year;
     if((year%4==0&&year%100!=0)||(year%400==0)){
         cout<<year<<" is a leap year.";

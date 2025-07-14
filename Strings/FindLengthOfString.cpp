00b196fcb4c68d9a1ae015074851579ed9a23cc1
Abhinav <psaiabhinav1724@gmail.com>
2025-07-14 14:33:40 +0530

    Length of a string added

---
+++ Strings/FindLengthOfString.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str="Sai Abhinav";
+    int lenght=str.length();
+    cout<<"Length of the string is: "<<lenght<<endl;
+    return 0;
+}
